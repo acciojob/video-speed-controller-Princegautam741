b6d50ad933c9aafb7311f4ed2dc57fa9a6b88736
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-23 14:46:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('video');
+const progress = document.querySelector('.progress__filled');
+const playButton = document.querySelector('.player__button');
+const volumeInput = document.querySelector('input[name="volume"]');
+const playbackSpeedInput = document.querySelector('input[name="playbackRate"]');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const rewindButton = document.querySelector('.rewind');
+const skipButton = document.querySelector('.skip');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playButton.textContent = '❚ ❚';
+  } else {
+    video.pause();
+    playButton.textContent = '►';
+  }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progress.style.flexBasis = `${percent}%`;
+}
+
+function handleVolumeChange() {
+  video.volume = volumeInput.value;
+}
+
+function handlePlaybackSpeedChange() {
+  video.playbackRate = playbackSpeedInput.value;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function rewind() {
+  video.currentTime -= 10;
+}
+
+function fastForward() {
+  video.currentTime += 25;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', () => (playButton.textContent = '❚ ❚'));
+video.addEventListener('pause', () => (playButton.textContent = '►'));
+video.addEventListener('timeupdate', handleProgress);
+
+playButton.addEventListener('click', togglePlay);
+volumeInput.addEventListener('input', handleVolumeChange);
+playbackSpeedInput.addEventListener('input', handlePlaybackSpeedChange);
+rewindButton.addEventListener('click', rewind);
+skipButton.addEventListener('click', fastForward);
+
+skipButtons.forEach((button) => button.addEventListener('click', skip));
